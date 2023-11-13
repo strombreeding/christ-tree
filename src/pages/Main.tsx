6c94454a266ab7753e80997d3d26226be6dfbb48
strombreeding <speaker1403@gmail.com>
2023-11-14 07:48:23 +0900

    gd

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components"
+
 const Header = () => {
     return (
         <button>
@@ -5,10 +7,39 @@ const Header = () => {
         </button>
     )
 }
+interface IDivProps {
+    width: string;
+    height: string;
+    bgImage?: string
+}
+const Background = styled.div < IDivProps>`
+max-width: 5000px;
+max-height: 5000px;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  background-color: lightblue;
+  background-image: ${(props) => props.bgImage ? `url(${props.bgImage})` : 'none'};
+  background-size: cover;
+  background-position: center;
+  z-index : 5;
+`;
 
+const Item = styled.div`
+    font-size: larger;
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    top: 1000px;
+    left: 2000px;
+    background-color: red;
+`
 const Main = () => {
     return (
-        <div>hello</div>
+        <>
+            <Background width="5000" height="5000" bgImage="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FZYFBv%2Fbtrn1gmFpGU%2F5p27RLkfXr0w8OdBG751CK%2Fimg.png" >
+                <Item>gd</Item>
+            </Background>
+        </>
     )
 }
 
